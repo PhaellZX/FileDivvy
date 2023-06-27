bffa35c70a5c27ef83e8b708903bbcbfe97db00f
PhaellZX <phaell.tavares@gmail.com>
2023-06-27 20:44:27 -0300

    Nova entrada

--- CreatePackages.py
+++ CreatePackages.py
@@ -1,8 +1,11 @@
 import os
 import shutil
+import tkinter as tk
+from tkinter import filedialog
+from PIL import ImageTk, Image
 
-def separar_imagens(pasta_origem, pasta_destino, imagens_por_pacote):
-    # Verifica se a pasta existe 
+def separar_imagens(pasta_origem, pasta_destino, imagens_por_pacote, nome_pasta):
+    # Verifica se a pasta existe
     if not os.path.exists(pasta_destino):
         os.makedirs(pasta_destino)
 
@@ -13,7 +16,7 @@ def separar_imagens(pasta_origem, pasta_destino, imagens_por_pacote):
     # Cria pacotes separados
     for i in range(0, total_imagens, imagens_por_pacote):
         # Cria uma nova pasta para o pacote
-        nome_pacote = f'Petrobras-EPIS_{i // imagens_por_pacote}'
+        nome_pacote = f'{nome_pasta}_{i // imagens_por_pacote}'
         pasta_pacote = os.path.join(pasta_destino, nome_pacote)
         os.makedirs(pasta_pacote)
 
@@ -23,11 +26,80 @@ def separar_imagens(pasta_origem, pasta_destino, imagens_por_pacote):
             imagem_destino = os.path.join(pasta_pacote, arquivos[j])
             shutil.copy(imagem_origem, imagem_destino)
 
-        print(f'Pacote {nome_pacote} criado com sucesso!')
+        print(f'Pacotes {nome_pacote} criado com sucesso!')
 
-# Execução do programa
-pasta_origem = 'C:/Users/Rapha/Desktop/TesteImg/images'
-pasta_destino = 'C:/Users/Rapha/Desktop/pacotes'
-imagens_por_pacote = 50 # define quantas imagens um pacote vai ter
+def selecionar_pasta_origem():
+    pasta_origem = filedialog.askdirectory()
+    pasta_origem_entry.delete(0, tk.END)
+    pasta_origem_entry.insert(tk.END, pasta_origem.replace("/", "\\"))
 
-separar_imagens(pasta_origem, pasta_destino, imagens_por_pacote)
+def selecionar_pasta_destino():
+    pasta_destino = filedialog.askdirectory()
+    pasta_destino_entry.delete(0, tk.END)
+    pasta_destino_entry.insert(tk.END, pasta_destino.replace("/", "\\"))
+
+def processar():
+    pasta_origem = pasta_origem_entry.get().replace("\\", "/")
+    pasta_destino = pasta_destino_entry.get().replace("\\", "/")
+    imagens_por_pacote = int(imagens_por_pacote_entry.get())
+    nome_pasta = nome_pasta_entry.get()
+
+    separar_imagens(pasta_origem, pasta_destino, imagens_por_pacote, nome_pasta)
+    status_label.config(text="Processamento concluído!")
+
+# Criação da janela principal
+window = tk.Tk()
+window.title("FPISeparator")
+window.geometry("400x300")
+
+# Componentes da GUI
+
+# Carregando a imagem de fundo
+image_path = "img/logo.png"  # Altere o caminho da imagem aqui
+background_image = Image.open(image_path)
+background_photo = ImageTk.PhotoImage(background_image)
+
+# Configurando um label com a imagem de fundo
+background_label = tk.Label(window, image=background_photo)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)      
+
+##############################################
+
+pasta_origem_label = tk.Label(window, text="Pasta de Origem:")
+pasta_origem_label.pack()
+
+pasta_origem_entry = tk.Entry(window, width=40)
+pasta_origem_entry.pack()
+
+selecionar_origem_button = tk.Button(window, text="Selecionar", command=selecionar_pasta_origem)
+selecionar_origem_button.pack()
+
+pasta_destino_label = tk.Label(window, text="Pasta de Destino:")
+pasta_destino_label.pack()
+
+pasta_destino_entry = tk.Entry(window, width=40)
+pasta_destino_entry.pack()
+
+selecionar_destino_button = tk.Button(window, text="Selecionar", command=selecionar_pasta_destino)
+selecionar_destino_button.pack()
+
+imagens_por_pacote_label = tk.Label(window, text="Número de Imagens por Pacote:")
+imagens_por_pacote_label.pack()
+
+imagens_por_pacote_entry = tk.Entry(window, width=10)
+imagens_por_pacote_entry.pack()
+
+nome_pasta_label = tk.Label(window, text="Nome da Pasta:")
+nome_pasta_label.pack()
+
+nome_pasta_entry = tk.Entry(window, width=40)
+nome_pasta_entry.pack()
+
+processar_button = tk.Button(window, text="Processar", command=processar)
+processar_button.pack()
+
+status_label = tk.Label(window, text="")
+status_label.pack()
+
+# Execução da janela principal
+window.mainloop()
--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-# Labeling Automation 🖼 📝 ⚙️
+# folder partition and images separator (FPISeparator) 🖼 📝 ⚙️
 Repositório criado para ajudar na automação das rotulagens dos datasets, organizando pastas, imagens, jsons e outras funções. 
-Toda ajuda é bem vinda! :)
+
+1 - Executar -> python3 CreatePackages.py
+
+
+2 - Pasta de Origem -> Selecionar diretório que contém as imagens
+3 - Pasta de Destino -> Selecionar diretório onde será dividido as pastas 
+4 - Números de Imagens por Pacote -> número de imagens em cada pasta
+5 - Nome das Pastas -> Na divisão de pastas será dividas pelo nome e no final o ID '_0,_1,_2,...'
+
+6 - Processar -> Iniciar a partição das pastas
 
 Repository created to help automate dataset labeling, organizing folders, images, jsons and other functions.
-Any help is welcome! :)
+
