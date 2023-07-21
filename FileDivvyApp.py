ad695caf863e01e56b45446bf723d897a544d129
Raphael <phaell.tavares@gmail.com>
2023-07-20 22:24:19 -0300

    Dados alterados

---
+++ FileDivvyApp.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+import os
+import subprocess
+
+def abrir_ImgFld():
+    # Abre o programa Main.py
+    subprocess.Popen(["python", "programs/ImgFldSeparator.py"])
+
+def abrir_videoFrames():
+    # Abre o programa Video_interface.py
+    subprocess.Popen(["python", "programs/VideoFrames.py"])
+
+def sair():
+    window.destroy()
+
+# Criação da janela principal
+window = tk.Tk()
+window.title("FileDivvy")
+window.geometry("400x300")
+window.resizable(False, False)
+window.configure(bg="#282C34")
+
+# Componentes da GUI
+titulo_label = tk.Label(window, text="FileDivvy", bg="#282C34", fg="#FFFFFF", font=("Arial", 16, "bold"))
+titulo_label.pack(pady=20)
+
+# Botão para abrir o programa Main.py
+botao_main = tk.Button(window, text="Separar imagens em várias pastas", command=abrir_ImgFld, font=("Arial", 12), bg="#000033", fg="#FFFFFF")
+botao_main.pack(pady=10)
+
+# Botão para abrir o programa Video_interface.py
+botao_video_interface = tk.Button(window, text="Separar video em varios frames", command=abrir_videoFrames, font=("Arial", 12), bg="#000033", fg="#FFFFFF")
+botao_video_interface.pack(pady=10)
+
+# Botão para sair do programa
+botao_sair = tk.Button(window, text="Sair", command=sair, font=("Arial", 14, "bold"), bg="#FF0000", fg="#FFFFFF")
+botao_sair.pack(pady=20)
+
+# Execução da janela principal
+window.mainloop()
